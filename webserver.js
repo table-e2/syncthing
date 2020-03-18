be3b358a955ffc4f68b00305ba84a49f3d23e744
William Baumgardner <wbaumgardner@ucdavis.edu>
2020-03-17 20:43:57 -0700

    basic website

---
+++ webserver.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const fs      = require('fs');
+const handlebars = require('express-handlebars')
+const app    = express();
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+const port   = 8080;
+const server = app.listen(port, () =>{
+  console.log("webserver started");
+});
+
+
+// landing page
+app.get('/', function(a_req, a_resp){
+  a_resp.render('home');
+});
