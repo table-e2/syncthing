3ceb04f3ad8dae5f618208c27fca4dab25b4da7b
William Baumgardner <wbaumgardner@ucdavis.edu>
2020-03-18 21:18:59 -0700

    uploading setup

--- webserver.js
+++ webserver.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const fs      = require('fs');
+const express    = require('express');
+const fs         = require('fs');
 const handlebars = require('express-handlebars')
-const app    = express();
+const app        = express();
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 
 const port   = 8080;
+
 const server = app.listen(port, () =>{
   console.log("webserver started");
 });
 
-
 // landing page
 app.get('/', function(a_req, a_resp){
   a_resp.render('home');
+});
+
+app.get('/upload', function(a_req, a_resp){
+  a_resp.render('upload');
+});
+
+app.get('/watch', function(a_req, a_resp){
+  a_resp.render('watch')
 });
